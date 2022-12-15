d0663adfe198ac2ae3c122c7bbb32b7fde3ea9df
itsKrish01 <72066631+Itskrish01@users.noreply.github.com>
2022-12-15 17:28:12 +0530

    new Changes

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import { Button, Container, Text } from "..";
-import Image from "next/image";
-import Link from "next/link";
 import { One } from "src/Templates/flaro/one";
 import { Two } from "src/Templates/flaro/two";
 import { Three } from "src/Templates/flaro/three";
